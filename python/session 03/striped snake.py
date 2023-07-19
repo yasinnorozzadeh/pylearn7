551bd5734b57b2c0d21c41f7905f193263297f20
Yasin Norozzadeh <88095232+yasinnorozzadeh@users.noreply.github.com>
2023-07-19 22:28:24 +0330

    Delete striped snake.py

--- python/session 03/striped snake.py
+++
@@ -1,9 +0,0 @@
-length_snake = int(input("enter length of snake:\n"))
-
-for i in range(length_snake):
-    if i // 2 * 2 == i:
-        print("*", end="", sep="")
-    else: 
-        print("#", end="")
-    
-print()
