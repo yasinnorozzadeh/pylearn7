c15358d20e707b1bfe2a95907f257aa717837ef1
Yasin Norozzadeh <88095232+yasinnorozzadeh@users.noreply.github.com>
2023-07-31 22:13:50 +0330

    Add files via upload

---
+++ python/sisson 06/practice/tic_tac_toe.py
@@ -0,0 +1,111 @@
+import random
+import time
+from termcolor import colored
+import pyfiglet
+
+# 😎
+def show_board(board):
+    for i in range(3):
+        for j in range(3):
+            print(colored(board[i][j], "cyan"), end=" ")
+        print()
+def check_replace(board, r, c):
+    if board[r][c] != "_":
+        print("this box is full!")
+        return False
+    else:
+        return True
+def enter_choice(player, board, char1, char2, g):
+    if g == 2 and player == 2:
+        while True:
+            player_r = random.randint(1, 3)
+            player_c = random.randint(1, 3)
+            if check_replace(board, player_r - 1, player_c - 1) == True:
+                break
+            else:
+                continue
+    else:
+        while True:
+            player_r = int(input(f"player{player} row: "))
+            player_c = int(input(f"player{player} colum: "))
+            if (0 < player_r < 4) and (0 < player_c < 4):
+                if check_replace(board, player_r - 1, player_c - 1) == True:
+                    break
+                else:
+                    continue
+            else:
+                print("index out off range!")
+                continue
+    if player == 1:
+        board[player_r - 1][player_c - 1] = char1
+    if player == 2:
+        board[player_r - 1][player_c - 1] = char2
+def check_game(board, player1, player2, g=1):
+    winer = ""
+    for r in range(3):
+        s1r = 0
+        s2r = 0
+        s1c = 0
+        s2c = 0
+        for c in range(3):
+            if  board[r][c] == player1:
+                s1r += 1
+            elif board[r][c] == player2:
+                s2r += 1
+            elif  board[c][r] == player1:
+                s1c += 1
+            elif board[c][r] == player2:
+                s2c += 1
+
+        if s1r == 3 or s1c == 3:
+            winer = "player1"
+        elif s2r == 3 or s2c == 3:
+            if g == 1:
+                winer = "player2"
+            else:
+                winer = "computer"
+    if board[0][0] == player1 and board[1][1] == player1 and board[2][2] == player1 or board[0][2] == player1 and board[1][1] == player1 and board[2][0] == player1:
+        winer = "player1"
+    elif board[0][0] == player2 and board[1][1] == player2 and board[2][2] == player2 or board[2][0] == player2 and board[1][1] == player2 and board[0][2] == player2:
+        winer = "player2"
+    c = 0
+    for i in range(3):
+        for j in range(3):
+            if not(board[i][j] == "_"):
+                c += 1
+    if c == 9 and winer == "":
+        winer = "equal"
+    return winer
+
+text = pyfiglet.figlet_format("welcome to Tic Tac Toe game", font="bubble")
+print(colored(text, "green"))
+board = [["_", "_", "_"], ["_", "_", "_"], ["_", "_", "_"]]
+start_t = time.time()
+game = int(input("1_player_vs_player\n2_player_vs_computer\nenter number of your choice: "))
+if game == 1:
+    player1_char = colored(input("player1 enter your charcter:\t"), "red")
+    player2_char = colored(input("player2 enter your character:\t"), "red")
+    winer = check_game(board, player1_char, player2_char, game)
+else:
+    player1_char = colored(input("player1 enter your charcter:\t"), "red")
+    player2_char = "#"
+    winer = check_game(board, player1_char, player2_char, game)
+while winer == "" and winer != "equal":
+    show_board(board)
+    enter_choice(1, board, player1_char, player2_char, game)
+    winer = check_game(board, player1_char, player2_char, game)
+    if winer != "":
+        continue
+    show_board(board)
+    enter_choice(2, board, player1_char, player2_char, game)
+    winer = check_game(board, player1_char, player2_char, game) 
+show_board(board)
+if winer == "equal":
+    text = "equal"
+else:
+    text = f"{winer} is winer"
+end_t = time.time()
+print("time: ", end_t - start_t)
+print(colored(text, "yellow"))
+text = pyfiglet.figlet_format("good bye!", font="doh")
+print(colored(text, "blue"))
